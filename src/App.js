7891476793fa616624d31973b832dad09d1a86fa
frank <frank.chen@skylinetw.com>
2019-02-18 18:02:08 +0800

    [update] add heading

--- src/App.js
+++ src/App.js
@@ -26,6 +26,7 @@ class App extends Component {
 
     return (
       <div className="container">
+        <h1>characters list</h1>
         <Table characterData={characters} 
                removeCharacter={this.removeCharacter}
         />
