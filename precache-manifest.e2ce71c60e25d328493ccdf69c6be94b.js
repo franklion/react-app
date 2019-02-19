68385952e86a73444ad01e28dc47c034cecc319f
frank <frank.chen@skylinetw.com>
2019-02-19 11:48:17 +0800

    Updates

---
+++ precache-manifest.e2ce71c60e25d328493ccdf69c6be94b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8ce05037bfd6015f067a",
+    "url": "/react-app/static/js/runtime~main.8ce05037.js"
+  },
+  {
+    "revision": "53b46ac7ff0c5b267f21",
+    "url": "/react-app/static/js/main.53b46ac7.chunk.js"
+  },
+  {
+    "revision": "1ad83593fd3178ca2fa5",
+    "url": "/react-app/static/js/2.1ad83593.chunk.js"
+  },
+  {
+    "revision": "53b46ac7ff0c5b267f21",
+    "url": "/react-app/static/css/main.c40dd0fe.chunk.css"
+  },
+  {
+    "revision": "90133e6c31b47879fea5b8cb56ba9aa8",
+    "url": "/react-app/index.html"
+  }
+];
